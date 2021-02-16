3f6de79e19661b3c983eb5fbbd3dc9ca2e57bc43
Justis Brown <justiscb@byu.edu>
2021-02-16 15:09:10 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustisBookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        public string ISBN { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public string classification { get; set; }
+        public string category { get; set; }
+        public float price { get; set; }
+    }
+}
